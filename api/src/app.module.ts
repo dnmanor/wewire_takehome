3d45f32d4b0f786e238be37d64fea45cec8f73cc
David Nuertey Manor <manornuertey@gmail.com>
2025-01-12 20:01:37 +0000

    add modules boilerplate

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConvertModule } from './convert/convert.module';
+import { ExchangeModule } from './exchange/exchange.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ConvertModule, ExchangeModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
