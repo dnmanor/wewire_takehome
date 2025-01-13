f23c5af1894f16d65e4aa49e46aeda4118e7cc2d
David Nuertey Manor <manornuertey@gmail.com>
2025-01-13 15:36:07 +0000

    add input and button components

---
+++ web/app/components/Button.tsx
@@ -0,0 +1,30 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant?: 'primary' | 'secondary' | 'destructive' | 'outline';
+}
+
+export default function Button({ children, className, variant, ...rest }: ButtonProps) {
+  const variantClasses = {
+    primary: 'bg-black hover:bg-gray-900 text-white',
+    secondary: 'bg-gray-200 hover:bg-gray-300 text-black',
+    destructive: 'bg-red-500 hover:bg-red-600 text-white',
+    outline: 'border border-gray-500 text-gray-500 hover:bg-gray-200',
+  };
+
+  return (
+    <button
+      className={`
+      inline-flex items-center justify-center whitespace-nowrap text-base 
+      h-10
+      w-auto
+      px-2
+      rounded-md
+      ${variant ? variantClasses[variant] : variantClasses.primary}
+      ${className}
+      `}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
